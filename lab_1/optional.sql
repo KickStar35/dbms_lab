f75c615926b1849bb90b89f87f91760dd80d04d1
KickStar35 <73743588+KickStar35@users.noreply.github.com>
2021-09-06 16:49:42 +0530

    Update optional.sql

--- lab_1/optional.sql
+++ lab_1/optional.sql
@@ -1,27 +1,13 @@
 1.
 mysql> select name, max(math+sci+eng+social+sports) from student;
 
-Output:
-+--------+---------------------------------+
-| name   | max(math+sci+eng+social+sports) |
-+--------+---------------------------------+
-| dwayne |                             434 |
-+--------+---------------------------------+
-
 2.
 mysql> select name, position("a" in name)=2 as Bool from student;
 
-Output:
-+--------+------+
-| name   | Bool |
-+--------+------+
-| dwayne |    0 |
-| john   |    0 |
-| dave   |    1 |
-| randy  |    1 |
-| kane   |    1 |
-| tom    |    0 |
-+--------+------+
+OR
+
+mysql> select name from student where name like '_a%';
 
 3.
+
 
