f807c0b1dba26a970a083c9eaa2b1c32d1cd58b2
KickStar35 <73743588+KickStar35@users.noreply.github.com>
2021-09-13 17:54:12 +0530

    Update assign2.sql

--- lab_2/assign2.sql
+++ lab_2/assign2.sql
@@ -10,21 +10,13 @@ mysql> select count(case when gender='M' then 1 end) as male,
 mysql> select count(*) as Student_num from student where math>(select avg(math+sci+social+eng+sports)/5 from student);
 
 --Suppose the university expansion team was looking at how they should expand further and inwhat areas of the country. As of now they currently need the ratio of campus capacity that is inthe south to the west zone of the country. (Like, use resource mentioned towards the end)
+mysql> select sum(cap) as south_zone from campus where pincode like '5%' or pincode like '6%';
+mysql> select sum(cap) as west_zone from campus where pincode like '3%' or pincode like '4%';
 
+--This year the college has decided to not only award the student who came first in the college(fromany batch/joining yrs) but also the student who has come second. Find the student who has thesecond highest avg in the table.(sub queries, aggregation, comparison, logical)
+mysql> select name from student where (math+sci+social+eng+sports)/5= (select max(math+sci+social+eng+sports)/5 from student where (math+sci+social+eng+sports)/5 <(select max(math+sci+social+eng+sports)/5 from student));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+--Display student’s name, rno, avg marks (“avg_marks”), score status (“score_status”) where scorestatus is “High” if above average and “Low” if below average. (sub queries)
 
 
 
