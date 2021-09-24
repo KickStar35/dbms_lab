4c7d640f28a1dd4b2804bcdd120b24c9f3ff1bbe
KickStar35 <73743588+KickStar35@users.noreply.github.com>
2021-09-24 19:49:56 +0530

    Update sqlhandler.py

--- lab_3/dbhandler/sqlhandler.py
+++ lab_3/dbhandler/sqlhandler.py
@@ -1,31 +1,26 @@
+import json
 import mysql.connector
-
-cnx = mysql.connector.connect(
-    user='root', password='Ilovemyson35', database='UserDB')
+cnx = mysql.connector.connect(user='root', password='asdf123', host='localhost', database='UserDB')
 cursor = cnx.cursor()
-
-
-class mysqlhandler:
-    def __init__(self):
-        pass
-
-    def add_user(name, phno, dob):
-        query = "insert into user_data(name, phno, dob) values(" + \
-            name+","+phno+","+dob+");"
-        cursor.execute(query)
+class Mysqlhandler:
+    def add_user(value):
+        sql="insert into user_data values(%s,%s,%s)"
+        cursor.execute(sql,value)
         cnx.commit()
-
-    def update_user(name, phno, dob):
-        query = "update user_data set name="+name+", dob="+dob+" where phno="+phno+";"
-        cursor.execute(query)
+    def update_user(value):
+        sql="update user_data set name=%s, dob=%s where phno=%s"
+        cursor.execute(sql,value)
         cnx.commit()
-
-    def delete_user(name, phno):
-        query = "delete from user_data where phno="+phno+";"
-        cursor.execute(query)
+    def delete_user(value):
+        sql="delete from user_data where name = %s"
+        cursor.execute(sql, value)
         cnx.commit()
-
-    def display_users(name):
-        query = "select * from user_data;"
-        cursor.execute(query)
-        cnx.commit()
+    def display_users():
+        b=[]
+        cursor.execute("select * from user_data")
+        result=cursor.fetchall()
+        for x in result:
+            a = json.dumps(x, default=str)
+            b.append(a)
+        c=json.dumps(b, indent=1)
+        return c
