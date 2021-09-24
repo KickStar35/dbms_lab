6071e5131d54ad93404c2913495f4826c682ee78
KickStar35 <73743588+KickStar35@users.noreply.github.com>
2021-09-24 19:50:22 +0530

    Update app.py

--- lab_3/flaskapp/app.py
+++ lab_3/flaskapp/app.py
@@ -1,1 +1,32 @@
+from werkzeug.wrappers import request
+from dbhandler.sqlhandler import Mysqlhandler
+import dbhandler
+from flask import *
+app = Flask(__name__)
+@app.route("/user/view",methods=['GET'])
+def view():
+    return Mysqlhandler.display_users()
 
+@app.route("/user/delete",methods=['POST','GET'])
+def delete():
+    value = request.get_json()
+    x=json.dumps(value)
+    y=json.loads(x)
+    z=(y["name"], )
+    return Mysqlhandler.delete_user(z)
+
+@app.route("/user/register",methods=['POST','GET'])
+def register():
+    value=request.get_json()
+    x=json.dumps(value)
+    y=json.loads(x)
+    z=(y["name"], int(y["phno"]), y["dob"])
+    return Mysqlhandler.add_user(z)
+
+@app.route("/user/update",methods=['POST','GET'])
+def update():
+    value=request.get_json()
+    x=json.dumps(value)
+    y=json.loads(x)
+    z=(y["name"], y["dob"], int(y["phno"]))
+    return Mysqlhandler.update_user(z)
